cbc64e0a21fa410870e83499f8e8e23169e7a9ea
Rostislav_Zhikharev <revers914@gmail.com>
2019-12-13 10:10:04 +0400

    add empty test class

---
+++ src/test/java/io/github/reversor/distance/AccuracyTest.java
@@ -0,0 +1,15 @@
+package io.github.reversor.distance;
+
+import net.sf.geographiclib.Geodesic;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class AccuracyTest {
+
+    @Test
+    @Disabled
+    void vincentyRulerTest() {
+
+    }
+
+}
