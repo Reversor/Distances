2c05a0a1f6b2f36c38628632d0e8cfa9b2d19660
Rostislav_Zhikharev <revers914@gmail.com>
2019-12-13 10:14:49 +0400

    add test dependencies and test class

---
+++ src/test/java/io/github/reversor/distance/AccuracyTest.java
@@ -0,0 +1,14 @@
+package io.github.reversor.distance;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class AccuracyTest {
+
+    @Test
+    @Disabled
+    void vincentyRulerTest() {
+
+    }
+
+}
